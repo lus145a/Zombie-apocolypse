7c00ce3f5fe5012878702da80ca1534cd7e56cd0
lus145a <lus145a@lyngbyprivateskole.dk>
2025-10-25 08:20:29 +0200

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,7 @@
+let name = game.askForString("what is your username", 36)
+let textSprite = textsprite.create("Hello Lucas, choose your charcer ")
+scaling.scaleToPixels(textSprite, 45, ScaleDirection.Uniformly, ScaleAnchor.Middle)
+textSprite.setPosition(80, 35)
+if (name == "#1647@sacul") {
+	
+}
